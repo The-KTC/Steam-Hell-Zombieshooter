345b9e0ed302ee05a4cd72bf47558cd64974d09d
The_KTC <kiwancakir@yahoo.de>
2022-03-18 18:58:36 +0100

    Einfach mal der Anfang

---
+++ Skripte/Spawnpoint.gd
@@ -0,0 +1,24 @@
+extends Node
+var Wave = 0
+var AnzZombiesTyp1 = 0   #  Anz steht für Anzahl
+var AnzZombiesTyp2 = 0   
+var AnzZombiesTyp3 = 0   
+
+
+func Zombies():
+	AnzZombiesTyp1
+	AnzZombiesTyp2
+	AnzZombiesTyp3
+
+
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
+#	if Zombies >= 1:
+#		pass
+#		var F1 = Zombie.instance()
+#		get_pa
+#		
+#		
