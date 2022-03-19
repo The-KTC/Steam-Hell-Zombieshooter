6c452b4e97562dba878249dedfe52df74002ca8b
Joshiii03 <67600895+Joshiii03@users.noreply.github.com>
2022-03-19 16:58:25 +0100

    Partikel verbesserung

--- Animation/Schusspartikel.gd
+++ Animation/Schusspartikel.gd
@@ -1,19 +1,10 @@
 extends Particles2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	print("parikel")
 	emitting = true
-	one_shot = true
-	pass # Replace with function body.
+	pass 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
