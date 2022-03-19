f44d642a78a0bef66c3fac55b7e5770e1278659a
The_KTC <kiwancakir@yahoo.de>
2022-03-19 13:10:37 +0100

    zooming

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,10 +7,15 @@ export var speed = 8
 func _ready():
 	pass 
 
+func _input(event):
+	if event.is_action_pressed("plus"):
+		$Camera2D.zoom.x -= 0.1
+		$Camera2D.zoom.y -= 0.1
+	if event.is_action_pressed("minus"):
+		$Camera2D.zoom.x += 0.1
+		$Camera2D.zoom.y += 0.1
+
 func _process(delta):
-	
-	
-	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
